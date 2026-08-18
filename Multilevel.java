b1403e3b289dfc721f092bc5366cffd06c472420
deepikadishadisha-rgb <deepikadishadisha@gmail.com>
2026-08-18 19:43:32 +0530

    Add files via upload

---
+++ Multilevel.java
@@ -0,0 +1,30 @@
+package day2;
+
+
+
+//Multilevel Inheritance
+
+class MultiGrandParent {
+
+ void bp() {
+     System.out.println("understain bp");
+ }
+}
+
+class MultiParent extends MultiGrandParent {
+
+ void cancer() {
+     System.out.println("understain");
+ }
+}
+
+public class Multilevel extends MultiParent {
+
+ public static void main(String[] args) {
+
+     Multilevel bb = new Multilevel();
+
+     bb.bp();
+     bb.cancer();
+ }
+}
