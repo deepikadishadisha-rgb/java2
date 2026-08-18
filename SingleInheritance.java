f70794721c2a4f73de15efed1138cde8973cc3c5
deepikadishadisha-rgb <deepikadishadisha@gmail.com>
2026-08-18 19:46:34 +0530

    Add files via upload

---
+++ SingleInheritance.java
@@ -0,0 +1,22 @@
+package day2;
+
+
+ public class SingleInheritance{
+
+    void cancer() {
+        System.out.println("Parent method: Cancer");
+    }
+
+    void bp() {
+        System.out.println("Parent method: BP");
+    }
+
+
+    public static void main(String[] args) {
+
+        SingleInheritance obj = new SingleInheritance();
+
+        obj.bp();
+        obj.cancer();
+    }
+}
