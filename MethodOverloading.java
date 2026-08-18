b825cb22790efa18df9d4d2d6f35f53fc0e7c75c
deepikadishadisha-rgb <deepikadishadisha@gmail.com>
2026-08-18 19:41:41 +0530

    Add files via upload

---
+++ MethodOverloading.java
@@ -0,0 +1,24 @@
+package day2;
+
+
+//Polymorphism
+//Method Overloading
+
+public class MethodOverloading {
+
+ void m1() {
+     System.out.println("Method without parameter");
+ }
+
+ void m1(int a) {
+     System.out.println("Method with one parameter: " + a);
+ }
+
+ public static void main(String[] args) {
+
+     MethodOverloading test = new MethodOverloading();
+
+     test.m1();
+     test.m1(3);
+ }
+}
